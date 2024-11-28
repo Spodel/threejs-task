12b81141c5868d845d48fc658a409186d3114a24
Spodel <vapsva125@gmail.com>
2024-11-28 08:32:31 +0500

    Added movement and restriction functional for cubes

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import { DragControls } from 'three/examples/jsm/controls/DragControls.js'
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(
@@ -7,11 +8,13 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 )
-
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
+const cubes = []
+
+// Создание нового куба
 function createCube() {
   const geometry = new THREE.BoxGeometry()
   const material = new THREE.MeshBasicMaterial({ color: '#FF6347' })
@@ -24,19 +27,43 @@ function createCube() {
   ) // Рандомно позиционируем кубы
 
   scene.add(cube)
+  cubes.push(cube)
   return cube
 }
 
 camera.position.z = 5
 
+/* Настройка перемещения кубов */
+
+const controls = new DragControls(cubes, camera, renderer.domElement) // Контроллер для перемещения по осям
+
+let selectedAxis = 'none'
+
+// Обработчик выбора оси для ограничения перемещения
+document.getElementById('axisSelect').addEventListener('change', (event) => {
+  selectedAxis = event.target.value
+})
+
+// Ограничиваем перемещение по выбранной оси
+controls.addEventListener('drag', function (event) {
+  if (event.object) {
+    if (selectedAxis === 'x') {
+      event.object.position.x = 0 // не получилось сделать, чтоб бралось текущее значение :( поэтому 0
+    } else if (selectedAxis === 'y') {
+      event.object.position.y = 0
+    } else if (selectedAxis === 'z') {
+      event.object.position.z = 0
+    }
+  }
+})
+
+// Обработчик добавления нового куба на сцену
+document.getElementById('addObjectBtn').addEventListener('click', () => {
+  createCube()
+})
+
 function animate() {
   requestAnimationFrame(animate)
   renderer.render(scene, camera)
 }
-
 animate()
-
-/** Добавление нового куба на сцену */
-document.getElementById('addObjectBtn').addEventListener('click', () => {
-  createCube()
-})
